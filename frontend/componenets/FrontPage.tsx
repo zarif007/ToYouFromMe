039b08b61026ce49f281e47b5f974f13e717be04
zarif007 <zarifhuq786@gmail.com>
2022-05-26 15:49:26 +0600

    Developed home page

--- frontend/componenets/FrontPage.tsx
+++ frontend/componenets/FrontPage.tsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { ethers } from "ethers";
 import abi from '../utils/PayloadPortal.json'
+import { useRecoilState } from 'recoil';
+import { currentUser } from '../atoms/currentUserAtom';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css'
+import { NextPage } from 'next';
 
-const FrontPage = () => {
 
+const FrontPage: NextPage = () => {
 
-  const [currentAccount, setCurrentAccount] = useState("");
+  const [currentAccount, setCurrentAccount] = useRecoilState(currentUser);
 
   const [allPayloads, setAllPayloads] = useState([]);
 
   const contractAddress = "0x64FE5a971EDB3497EC2610B1A3fEbA72bb1DCcC7";
 
   const contractABI = abi.abi;
+
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -129,8 +135,8 @@ const FrontPage = () => {
     checkIfWalletIsConnected();
   }, [])
   return (
-    <>
-      {!currentAccount && (
+    <div>
+      {/* {!currentAccount && (
           <button className="" onClick={connectWallet}>
             Connect Wallet
           </button>
@@ -141,9 +147,38 @@ const FrontPage = () => {
            getAllPayloads();
        }}>Send</button>
 
-       <p>{allPayloads.length}</p>
-    </>
-  )
+       <p>{allPayloads.length}</p> */}
+
+      <section className="py-20 lg:py-28">
+        <div className="container mx-auto px-5 text-center">
+          <div className="mb-16">
+            <div className="space-y-4 mb-12">
+              <h4 className="text-2xl sm:text-3xl font-semibold text-blue-700">
+                Best Game Shop
+              </h4>
+              <h1 className="text-5xl sm:text-7xl font-bold text-white uppercase">
+                Buy Collect Play
+              </h1>
+              <p className="sm:w-3/4 lg:w-2/4 xl:w-96 mx-auto text-lg sm:text-xl text-gray-600">
+                Get favourite Games from here
+              </p>
+            </div>
+            <button
+              className="px-12 py-4 bg-blue-700 hover:bg-blue-800 transition duration-100 ease-in-out shadow hover:shadow-lg transform hover:scale-110 text-white font-semibold text-lg rounded-full inline-block"
+            >
+              Buy now
+            </button>
+          </div>
+          <img
+            className="mx-auto xl:max-w-screen-lg mb-28"
+            src="https://i.ibb.co/strf13Y/Untitled-designu.png"
+            alt="Monster"
+          />
+        </div>
+      </section>
+
+    </div>
+  );
 }
 
 export default FrontPage
