a0a0f176cb1553b5d8f66cfc8347171bd7aaca8d
zarif007 <zarifhuq786@gmail.com>
2022-05-27 02:52:40 +0600

    Developing [id] comp

--- frontend/pages/profile/[id].tsx
+++ frontend/pages/profile/[id].tsx
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil';
 import abi from '../../utils/PayloadPortal.json'
 import { currentUser } from './../../atoms/currentUserAtom';
+import { LinkPreview } from '@dhaiwat10/react-link-preview';
+import Head from 'next/head';
+
 
 const Profile = () => {
 
@@ -12,6 +15,8 @@ const Profile = () => {
   const [allPayloads, setAllPayloads] = useState([]);
 
   const [currentAccount, setCurrentAccount] = useRecoilState<string>(currentUser);
+
+  const [inputs, setInputs] = useState<{text: string, url: string}>({text: '', url: ''})
 
   const contractAddress = "0xD3E56888702C07D46BFa44570F00F7B373491573";
 
@@ -59,7 +64,7 @@ const Profile = () => {
         * Execute the actual payload from your smart contract
         */
        console.log(currentAccount)
-        const payloadTxn = await payloadPortalContract.payload(currentAccount, "kire", "dsdsadsa");
+        const payloadTxn = await payloadPortalContract.payload(currentAccount, inputs.text, inputs.url);
         console.log("Mining...", payloadTxn.hash);
 
         await payloadTxn.wait();
@@ -67,6 +72,8 @@ const Profile = () => {
 
         count = await payloadPortalContract.getTotalPayloads();
         console.log("Retrieved total payload count...", count.toNumber());
+
+        setInputs({text: '', url: ''})
 
         getAllPayloads()
       } else {
@@ -119,28 +126,66 @@ const Profile = () => {
   }, [])
 
   return (
-    <div className='bg-black min-h-screen text-white py-48'>
-      <div className='md:text-3xl text-xl font-bold text-[#ADD8E6] uppercase flex flex-col justify-center items-center'>
-          <h1>
-              {
-                  id === currentAccount ? 'Your Profile' : 'Sending to ⤵️'
-              }
+    <>
+
+      <Head>
+        <title>{id}</title>
+        <meta name="description" content={`${id}`} />
+        <link rel="icon" href="https://i.ibb.co/Fb3TqKH/to-you-removebg-preview.png" />
+      </Head>
+
+      <div className='bg-black min-h-screen text-white md:py-48 py-12'>
+
+        <div className='md:text-3xl text-xl font-bold text-[#ADD8E6] uppercase flex flex-col justify-center items-center'>
+            <h1 className='mb-1'>
+                {
+                    id === currentAccount ? 'Your Profile 😀' : 'Sending to ⤵️'
+                }
             </h1>
-          <h1>{id}</h1>
+            <h1 className='md:text-3xl text-sm bg-gray-700 py-2 px-2 rounded-md'>{id}</h1>
+        </div>
+
+        {/* Inputs  */}
+        <div className="max-w-3xl md:mx-auto ml-3 mr-3 mt-12 flex justify-center flex-col">
+          <label className="block mb-2 text-lg font-semibold text-gray-900 dark:text-gray-300">Message📝</label>
+          <div className="relative mb-6">
+            <input type="text" 
+              className="bg-gray-50 font-semibold  text-gray-900 text-sm rounded-lg  block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white ring-0" placeholder="U Cute ;)" 
+              onChange={(e: any) => inputs.text = e.target.value}/>
+          </div>
+          <label className="block mb-2 text-lg font-semibold text-gray-900 dark:text-gray-300">Media(Video, Gif, Meme, Image)❤️‍🔥</label>
+
+          <div className="flex">
+            <input type="url" 
+            className="bg-gray-50 font-semibold  text-gray-900 text-sm rounded-lg  block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white ring-0" placeholder="URL" 
+            onChange={(e: any) => inputs.url = e.target.value}/>
+          </div>
+        </div>
+
+        
+        <div className='mx-auto flex justify-center mt-2'>
+          <button
+            type="button"
+            className="m-2 rounded px-8 py-2 font-semibold text-gray-700 bg-[#ADD8E6] hover:bg-[#79bad0] text-lg"
+            onClick={sendPayload}
+          >
+            Send
+          </button>
+        </div>
+        {/* <LinkPreview url='https://giphy.com/stories/8-gifs-from-the-2022-uefa-womens-champions-league-final-740cffd3-2338' width='400px' backgroundColor='black' borderColor='#0f172a' primaryTextColor='#94a3b8' /> */}
+        
+        {
+            allPayloads.map((p: any, index) => {
+                return (
+                  <h1 key={index} className='md:text-3xl text-xl font-bold text-[#ADD8E6] uppercase flex flex-col justify-center items-center'>
+                      {p.text}
+                  </h1>
+                )
+            })
+        }
+
       </div>
-      {
-          allPayloads.map((p: any, index) => {
-              return (
-                <h1 key={index} className='md:text-3xl text-xl font-bold text-[#ADD8E6] uppercase flex flex-col justify-center items-center'>
-                    {p.text}
-                </h1>
-              )
-          })
-      }
-      {
-        (currentAccount !== undefined || '') && <button onClick={sendPayload} >send</button>
-      }
-    </div>
+    </>
   )
 }
 
